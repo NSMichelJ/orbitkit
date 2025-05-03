051a707c72b28f3a4f010d6aa89e91e3113ea10a
NSMichelJ <nunezsalmeronm@gmail.com>
2025-05-03 08:07:32 -0400

    feat(cli): update package.json and tsconfig for improved module support and dependencies

--- packages/cli/tsup.config.ts
+++ packages/cli/tsup.config.ts
@@ -2,7 +2,8 @@ import { defineConfig } from "tsup";
 
 export default defineConfig({
   entry: ["src/*"],
-  format: "cjs",
+  format: ["esm"],
+  dts: true,
   splitting: false,
   sourcemap: true,
   clean: true,
