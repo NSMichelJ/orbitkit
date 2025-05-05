18d68328d55c17ff146bae212a8d73d83700ce97
NSMichelJ <nunezsalmeronm@gmail.com>
2025-05-04 22:44:51 -0400

    fix(add): update components handling to improve type safety and add writeConfig call

--- packages/cli/src/commands/add.ts
+++ packages/cli/src/commands/add.ts
@@ -5,8 +5,10 @@ import { Command } from "commander";
 import fs from "fs-extra";
 
 import readConfig from "@/utils/readConfig";
+import writeConfig from "@/utils/writeConfig";
 import {
   checkComponentsInRegistry,
+  ComponentRegistryEntry,
   getAllComponents,
   getComponentPath,
 } from "@orbitui/components";
@@ -29,12 +31,14 @@ export const add = new Command()
     }
 
     const PATH = process.cwd();
-    const componentsToInstall: string[] = [];
+    const componentsToInstall: ComponentRegistryEntry[] = [];
 
     if (!components.length) {
-      const availableComponents = getAllComponents().map(
-        (component) => component.name,
-      );
+      const availableComponents = getAllComponents().map((component) => ({
+        name: component.name,
+        value: component,
+      }));
+
       const { selectedComponents } = await inquirer.prompt([
         {
           type: "checkbox",
@@ -49,7 +53,7 @@ export const add = new Command()
         return;
       }
 
-      componentsToInstall.push(...(selectedComponents as string[]));
+      componentsToInstall.push(...selectedComponents);
     }
 
     const { valid: validComponents, invalid: invalidComponents } =
@@ -70,7 +74,7 @@ export const add = new Command()
     log.ln();
     log.info("The following components will be added to your project:");
     for (const componentToInstall of componentsToInstall) {
-      log.blank(`- ${componentToInstall}`);
+      log.blank(`- ${componentToInstall.name}@${componentToInstall.version}`);
     }
 
     const config = await readConfig();
@@ -81,16 +85,25 @@ export const add = new Command()
 
     const spinner = ora("Adding component...").start();
     for (const componentToInstall of componentsToInstall) {
-      const componentPath = getComponentPath(componentToInstall);
-      const componentsDir = path.join(
-        PATH,
-        componentsDirConfig,
-        componentToInstall,
-      );
+      const { name, version } = componentToInstall;
 
-      spinner.text = `Adding ${componentToInstall}...`;
+      if (config.components?.find((c) => c.name === name)) {
+        continue;
+      }
+
+      const componentPath = getComponentPath(name);
+      const componentsDir = path.join(PATH, componentsDirConfig, name);
+
+      spinner.text = `Adding ${componentToInstall.name}@${componentToInstall.version}...`;
       try {
         await fs.copy(componentPath, componentsDir);
+        config.components = config.components || [];
+
+        config.components.push({
+          name: name,
+          version: version,
+        });
+        await writeConfig(config);
       } catch {
         log.error(`Error adding component ${componentToInstall}`);
       }
