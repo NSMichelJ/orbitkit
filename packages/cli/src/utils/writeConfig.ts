d16bf38b23c7902a158244447005ae9833dc8d08
NSMichelJ <nunezsalmeronm@gmail.com>
2025-05-04 22:42:25 -0400

    fix(writeConfig): make writeConfig function asynchronous for proper file handling

--- packages/cli/src/utils/writeConfig.ts
+++ packages/cli/src/utils/writeConfig.ts
@@ -3,10 +3,10 @@ import fs from "fs-extra";
 import path from "node:path";
 import { ORBIT_CONFIG_FILE_NAME } from "./constants";
 
-export default function writeConfig(config: Config) {
+export default async function writeConfig(config: Config) {
   try {
     const file = path.join(process.cwd(), ORBIT_CONFIG_FILE_NAME);
-    fs.outputJSON(file, config, {
+    await fs.outputJSON(file, config, {
       encoding: "utf-8",
       spaces: 2,
     });
