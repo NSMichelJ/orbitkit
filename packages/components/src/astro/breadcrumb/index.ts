58a953b9ad16cd0a65895ca12c18ae15d5c006c6
NSMichelJ <nunezsalmeronm@gmail.com>
2025-05-10 07:44:35 -0400

    feat(breadcrumb): add breadcrumb components

---
+++ packages/components/src/astro/breadcrumb/index.ts
@@ -0,0 +1,17 @@
+import Breadcrumb from "./Breadcrumb.astro";
+import BreadcrumbEllipsis from "./BreadcrumbEllipsis.astro";
+import BreadcrumbItem from "./BreadcrumbItem.astro";
+import BreadcrumbLink from "./BreadcrumbLink.astro";
+import BreadcrumbList from "./BreadcrumbList.astro";
+import BreadcrumbPage from "./BreadcrumbPage.astro";
+import BreadcrumbSeparator from "./BreadcrumbSeparator.astro";
+
+export {
+  Breadcrumb,
+  BreadcrumbEllipsis,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+};
