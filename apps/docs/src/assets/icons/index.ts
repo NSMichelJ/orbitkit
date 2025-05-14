b9d979b4dbe3fd3c171c260e0ccbc2931930c2fe
NSMichelJ <nunezsalmeronm@gmail.com>
2025-05-14 17:35:45 -0400

    feat: add new SVG icons and index file for asset management

---
+++ apps/docs/src/assets/icons/index.ts
@@ -0,0 +1,31 @@
+import BlocksIcon from "./blocks.svg";
+import BrandGitHubIcon from "./brand-github.svg";
+import CircleCheckIcon from "./circle-check.svg";
+import CircleIcon from "./circle.svg";
+import ClockIcon from "./clock.svg";
+import CubeIcon from "./cube.svg";
+import HeartHandshakeIcon from "./heart-handshake.svg";
+import PackageOffIcon from "./package-off.svg";
+import PalleteIcon from "./palette.svg";
+import RocketIcon from "./rocket.svg";
+import ShieldIcon from "./shield-half.svg";
+import SparklesIcon from "./sparkles.svg";
+import StartIcon from "./star.svg";
+import TerminalIcon from "./terminal-2.svg";
+
+export {
+  BlocksIcon,
+  BrandGitHubIcon,
+  CircleCheckIcon,
+  CircleIcon,
+  ClockIcon,
+  CubeIcon,
+  HeartHandshakeIcon,
+  PackageOffIcon,
+  PalleteIcon,
+  RocketIcon,
+  ShieldIcon,
+  SparklesIcon,
+  StartIcon,
+  TerminalIcon,
+};
