1207bf59d8428bf06426fce28dc977a27582e717
NSMichelJ <nunezsalmeronm@gmail.com>
2025-05-15 00:30:59 -0400

    feat(docs): configure Starlight

--- apps/docs/astro.config.mjs
+++ apps/docs/astro.config.mjs
@@ -2,8 +2,16 @@
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
+import starlight from "@astrojs/starlight";
+
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
+  integrations: [
+    starlight({
+      title: "OrbitUI",
+    }),
+  ],
 });
