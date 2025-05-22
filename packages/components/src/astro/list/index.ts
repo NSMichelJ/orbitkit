ba6cf0bb46de0afef91c9fab6f5f84015a2b4897
NSMichelJ <nunezsalmeronm@gmail.com>
2025-05-21 21:31:16 -0400

    feat(list): add List and ListItem components

---
+++ packages/components/src/astro/list/index.ts
@@ -0,0 +1,5 @@
+import List from "./List.astro";
+import ListItem from "./ListItem.astro";
+import { listItemVariants, listVariants } from "./ListVariants";
+
+export { List, ListItem, listItemVariants, listVariants };
