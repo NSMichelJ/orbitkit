9fefa0341cdf4f71c8f45ec10e51c9bf6bb419d2
nsmichelj <nunezsalmeronm@gmail.com>
2025-06-20 17:20:59 -0400

    feat(cli): add overwrite option to add command

--- packages/cli/src/commands/add.ts
+++ packages/cli/src/commands/add.ts
@@ -22,6 +22,7 @@ export const add = new Command()
   .description("Add Orbit components to your project")
   .argument("[components...]", "The components to add")
   .option("-a, --all", "Add all available Orbit components", false)
+  .option("-o, --overwrite", "Overwrite existing components.", false)
   .option(
     "-w, --working-directory <path>",
     "Specify the working directory path",
@@ -61,6 +62,12 @@ export const add = new Command()
       }
     }
 
+    const config = await readConfig(workingDirectory);
+    if (!config?.componentsDir) {
+      log.error("Components directory not configured.");
+      return;
+    }
+
     const componentsToInstall: ComponentRegistryEntry[] =
       await promptForComponents({ components, ...args });
 
@@ -68,48 +75,76 @@ export const add = new Command()
       return;
     }
 
-    log.ln();
-    log.info("The following components will be added to your project:");
-    for (const componentToInstall of componentsToInstall) {
-      log.blank(`- ${componentToInstall.name}@${componentToInstall.version}`);
-    }
+    config.components = config.components || [];
 
-    const config = await readConfig(workingDirectory);
-    if (!config?.componentsDir) {
+    const ignoredComponents: ComponentRegistryEntry[] = [];
+
+    const filteredComponentsToInstall = componentsToInstall.filter(
+      (componentToInstall) => {
+        const exists = config.components?.some(
+          (c) => c.name === componentToInstall.name,
+        );
+        if (exists && !args.overwrite) {
+          ignoredComponents.push(componentToInstall);
+          return false;
+        }
+        return true;
+      },
+    );
+
+    if (!filteredComponentsToInstall.length) {
+      if (ignoredComponents.length) {
+        log.ln();
+        log.warn(
+          "The following components are already installed. Use --overwrite to overwrite:",
+        );
+        ignoredComponents.forEach((c) => log.blank(`- ${c.name}@${c.version}`));
+      }
       return;
     }
+
+    log.ln();
+    log.info("The following components will be added to your project:");
+    filteredComponentsToInstall.forEach((componentToInstall) => {
+      log.blank(`- ${componentToInstall.name}@${componentToInstall.version}`);
+    });
+
     const componentsDirConfig = config.componentsDir;
-
     const spinner = ora("Adding component...").start();
-    for (const componentToInstall of componentsToInstall) {
+
+    for (const componentToInstall of filteredComponentsToInstall) {
       const { name, version } = componentToInstall;
-
-      if (config.components?.find((c) => c.name === name)) {
-        continue;
-      }
-
+      const componentIndex = config.components.findIndex(
+        (c) => c.name === name,
+      );
       const componentPath = getComponentPath(name);
       const componentsDir = path.join(
         workingDirectory,
         componentsDirConfig,
         name,
       );
-
-      spinner.text = `Adding ${componentToInstall.name}@${componentToInstall.version}...`;
+      spinner.text = `Adding ${name}@${version}...`;
       try {
-        await fs.copy(componentPath, componentsDir);
-        config.components = config.components || [];
-
-        config.components.push({
-          name: name,
-          version: version,
-        });
-        await writeConfig(workingDirectory, config);
+        await fs.copy(componentPath, componentsDir, { overwrite: true });
+        if (componentIndex !== -1) {
+          config.components[componentIndex] = { name, version };
+        } else {
+          config.components.push({ name, version });
+        }
       } catch {
-        log.error(`Error adding component ${componentToInstall}`);
+        log.error(`Error adding component ${name}`);
       }
     }
+    await writeConfig(workingDirectory, config);
     spinner.stop();
+
+    if (ignoredComponents.length) {
+      log.ln();
+      log.warn(
+        "The following components are already installed. Use --overwrite to overwrite:",
+      );
+      ignoredComponents.forEach((c) => log.blank(`- ${c.name}@${c.version}`));
+    }
   });
 
 async function promptForComponents(opts: { components: string[]; all: true }) {
